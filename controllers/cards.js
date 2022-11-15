ddb553bb21a32508d14d6fa39b56de7bcf412b62
SonjaKuv <kuvshinova.sonja@yandex.ru>
2022-11-15 19:15:59 +0300

    add errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,38 +1,96 @@
+const { StatusCodes } = require('http-status-codes');
+const BadRequestError = require('../errors/BadRequest');
+const NotFoundError = require('../errors/NotFound');
+const InternalServerError = require('../errors/InternalServer');
 const Card = require('../models/cards');
 
-module.exports.getCards = (req, res) => {
+module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((cards) => res.status(StatusCodes.OK).send({ data: cards }))
+    .catch((err) => {
+      throw err;
+    })
+    .catch(next);
 };
 
-module.exports.createCard = (req, res) => {
+module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => res.status(StatusCodes.CREATED).send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при создании карточки'));
+      } else {
+        next(new InternalServerError('Произошла внутренняя ошибка сервера'));
+      }
+      next(err);
+    });
 };
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   Card.findByIdAndRemove(req.params.id)
-    .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((card) => {
+      if (card === null) {
+        throw new NotFoundError('Карточка c указанным id не найдена');
+      }
+      return card;
+    })
+    .then((card) => card.delete())
+    .then((data) => res.send(data))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Передан некорректный формат id'));
+      } else {
+        next(new InternalServerError('Произошла внутренняя ошибка сервера'));
+      }
+      next(err);
+    });
 };
 
-module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
+module.exports.likeCard = (req, res, next) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
   { new: true },
 )
-  .then((cards) => res.send({ data: cards }))
-  .catch((err) => res.status(500).send({ message: err.message }));
+  .then((card) => {
+    if (card === null) {
+      throw new NotFoundError('Передан несуществующий id карточки');
+    } else {
+      res.send({ data: card });
+    }
+  })
+  .catch((err) => {
+    if (err.name === 'CastError') {
+      next(new BadRequestError('Передан некорректный формат id'));
+    } else if (err.name === 'ValidationError') {
+      next(new BadRequestError('Переданы некорректные данные для лайка'));
+    } else {
+      next(new InternalServerError('Произошла внутренняя ошибка сервера'));
+    }
+    next(err);
+  });
 
-module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
+module.exports.dislikeCard = (req, res, next) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } }, // убрать _id из массива
   { new: true },
 )
-  .then((cards) => res.send({ data: cards }))
-  .catch((err) => res.status(500).send({ message: err.message }));
+  .then((card) => {
+    if (card === null) {
+      throw new NotFoundError('Передан несуществующий id карточки');
+    } else {
+      res.send(card);
+    }
+  })
+  .catch((err) => {
+    if (err.name === 'CastError') {
+      next(new BadRequestError('Передан некорректный формат id'));
+    } else if (err.name === 'ValidationError') {
+      next(new BadRequestError('Переданы некорректные данные для удаления лайка'));
+    } else {
+      next(new InternalServerError('Произошла внутренняя ошибка сервера'));
+    }
+    next(err);
+  });
--- controllers/users.js
+++ controllers/users.js
@@ -1,41 +1,95 @@
+const { StatusCodes } = require('http-status-codes');
+const BadRequestError = require('../errors/BadRequest');
+const NotFoundError = require('../errors/NotFound');
+const InternalServerError = require('../errors/InternalServer');
 const User = require('../models/users');
 
-module.exports.getUsers = (req, res) => {
+module.exports.getUsers = (req, res, next) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((users) => res.status(StatusCodes.OK).send({ data: users }))
+    .catch((err) => {
+      throw err;
+    })
+    .catch(next);
 };
 
-module.exports.createUser = (req, res) => {
+module.exports.createUser = (req, res, next) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((users) => res.status(StatusCodes.CREATED).send({ data: users }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при создании пользователя'));
+      } else {
+        next(new InternalServerError('Произошла внутренняя ошибка сервера'));
+      }
+      next(err);
+    });
 };
 
-module.exports.getUserById = (req, res) => {
-  User.findById(req.params)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+module.exports.getUserById = (req, res, next) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .then((user) => {
+      if (user === null) {
+        throw new NotFoundError('Запрашиваемый пользователь не найден');
+      } else {
+        res.send(user);
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError('Передан некорректный формат id'));
+      } else {
+        next(new InternalServerError('Произошла внутренняя ошибка сервера'));
+      }
+      next(err);
+    });
 };
 
-module.exports.setNewProfileInfo = (req, res) => {
+module.exports.setNewProfileInfo = (req, res, next) => {
   User.findByIdAndUpdate(
     req.user._id,
     { name: req.params.name, about: req.params.about },
-    { new: true },
+    { new: true, runValidators: true },
   )
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => {
+      if (user === null) {
+        throw new NotFoundError('Запрашиваемый пользователь не найден');
+      } else {
+        res.send(user);
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при обновлении профиля'));
+      } else {
+        next(new InternalServerError('Произошла внутренняя ошибка сервера'));
+      }
+      next(err);
+    });
 };
 
-module.exports.setNewAvatar = (req, res) => {
+module.exports.setNewAvatar = (req, res, next) => {
   User.findByIdAndUpdate(
     req.user._id,
     { avatar: req.params.avatar },
     { new: true },
   )
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => {
+      if (user === null) {
+        throw new NotFoundError('Запрашиваемый пользователь не найден');
+      } else {
+        res.send(user);
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при обновлении аватара'));
+      } else {
+        next(new InternalServerError('Произошла внутренняя ошибка сервера'));
+      }
+      next(err);
+    });
 };
