7876e6099a74e4ac4837b95691d2b81eaf4b7567
SonjaKuv <kuvshinova.sonja@yandex.ru>
2022-11-25 16:29:14 +0300

    autotest

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ app.use(routerCards);
 app.use(wrongPath);
 app.use(errors());
 app.use((err, req, res, next) => {
-  const { statusCode = StatusCodes.INTERNAL_SERVER_ERROR, message } = err;
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-    message: statusCode === StatusCodes.INTERNAL_SERVER_ERROR ? 'На сервере произошла ошибка' : message,
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
   next();
 });
